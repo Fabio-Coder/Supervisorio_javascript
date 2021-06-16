d4a1a45809b6f5111b15426f6be343d81ff68e6f
Fabio-Coder <0xfab1015@gmail.com>
2021-06-16 08:22:13 -0300

    Crud realizdo com sucesso.

--- js/supervisorio.js
+++ js/supervisorio.js
@@ -2,21 +2,22 @@ const supervisorio_database = {};
 
 (function (){
     let machine_id = false;
+    let machine_name = '';
 
     function new_machine(machine, production, status) {
         const machine_data = {
             machine: machine,
             production: production,
             status: status,
-            createdat: firebase.database.ServerValue.TIMESTAMP,
+            createDate: firebase.database.ServerValue.TIMESTAMP,
         };
+
+        let machine_name = machine.replace(/[^0-9]/, '');  
 
         if (!machine_id){
             machine_id = firebase.database().ref().child('machines' + machine_name).push().key;
         }
-        
-        let machine_name = machine.replace(/[^0-9]/, '');    
-
+            
         let updates = {};
         updates['/machines/' + machine_name] = machine_data;
 
@@ -32,7 +33,7 @@ const supervisorio_database = {};
     }
 
     function remove_machine(machine_name) {
-        if (!machine_id) return {success:false, message: 'Invalid machine.'};
+        if (!machine_name) return {success:false, message: 'Invalid machine.'};
 
         let machine_ref = firebase.database().ref('/machines/' + machine_name);
 
@@ -45,15 +46,16 @@ const supervisorio_database = {};
             });
     }
 
-    function update_machine(machine_name) {
+    function update_machine(machine_name, production, status) {
         if (!machine_id) return {success:false, message: 'Invalid machine.'};
 
         let machine_ref = firebase.database().ref('/machines/' + machine_name);
         
         let updates = {};
-        updates['/machine'] = machine;
-        updates['/lastUpdate'] = firebase.database.ServerValue.TIMESTAMP;
-
+        updates['/production'] = production,
+        updates['/status'] = status,
+        updates['/lastUpdate'] = firebase.database.ServerValue.TIMESTAMP,
+        
         machine_ref.update(updates)
             .then(function(){
                 return {success:true, message: 'Machine updated.'}; 
@@ -64,12 +66,51 @@ const supervisorio_database = {};
     }
 
     function reset_machine() {
-        if (!machine_id) return {success:false, message: 'Invalid machine.'};
+        if (!machine_name) return {success:false, message: 'Invalid machine.'};   
     }
+
+    //var starCountRef = firebase.database().ref('posts/' + postId + '/starCount');
+    //starCountRef.on('value', (snapshot) => {
+    //const data = snapshot.val();
+    //updateStarCount(postElement, data);
+    //});
+
+
+    async function listen_machines(){
+        if (!machine_id) return {success:false, message: 'Invalid machine.'};
+        
+        let machine_ref = firebase.database().ref('/machines/' + machine_name);
+
+        machine_ref.on('value',(snapshot) => {
+            const data = snapshot.val();
+            console.log('Production changed: ', snapshot.val());
+        });
+            // .then(function(snapshot) {
+            //     //Production
+            //     console.log('Production changed: ', snapshot.val());
+            //     if(snapshot.key == 'production') {
+            //         console.log('Production changed: ', snapshot.val());
+            //         return {success: true, message: 'Production updated.', data: snapshot.val()};
+            //     //Status
+            //     } else if(snapshot.key == 'status'){
+            //         console.log('Status changed: ', snapshot.val());
+            //         return {success: true, message: 'Status updated.', data: snapshot.val()};
+            //     }
+
+            // })
+            // .catch(function(error){
+            //     return {success:false, message: 'Machine update failed: ${error.message}'}; 
+            // });
+    }
+
+
+
 
     supervisorio_database.new = new_machine;
     supervisorio_database.remove = remove_machine;
     supervisorio_database.update = update_machine;
     supervisorio_database.reset = reset_machine;
+    supervisorio_database.listen = listen_machines;
 
 })()
+
